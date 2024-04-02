71e2fb2fd7f561ef0e25caf6d2ca077f5c511886
jamiel00800 <jrsumeracruz@iskolarngbayan@pup.edu.ph>
2024-04-02 15:41:35 +0800

    headeer

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   
+    $('#NavBar').load("./components/NavBar.html");
+    $('#Footer').load("./components/footer.html");
+    $('#Hero').load("./pages/header.html");
+ 
+ 
+ 
+ });
